b27d701debdc6e9c7d48e80a99ec7dea63e37ea8
Xander Harris <duchess@gahan-corporation.com>
2017-12-05 03:37:31 +0000

    Replace odoo with gerp. See #10.

---
+++ gerp/addons/test_main_flows/tests/test_flow.py
@@ -0,0 +1,11 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import gerp.tests
+
+
+@gerp.tests.common.at_install(False)
+@gerp.tests.common.post_install(True)
+class TestUi(gerp.tests.HttpCase):
+    
+    def test_01_main_flow_tour(self):
+        self.phantom_js("/web", "gerp.__DEBUG__.services['web_tour.tour'].run('main_flow_tour')", "gerp.__DEBUG__.services['web_tour.tour'].tours.main_flow_tour.ready", login="admin", timeout=180)
