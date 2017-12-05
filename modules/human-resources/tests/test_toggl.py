d8235cd6eb80287af3955e2c86ed850e74bc7b42
Xander Harris <duchess@gahan-corporation.com>
2017-12-05 07:02:23 +0000

    Add model file. See #10.

--- modules/human-resources/tests/test_toggl.py
+++ modules/human-resources/tests/test_toggl.py
@@ -9,6 +9,18 @@ class TestToggl(object):
         """Ensure the api token works."""
         assert self.token is True
 
+    def test_get_project(self):
+        """Test if the correct Toggl project is returend"""
+        assert self.token is True
+
     def test_get_entry(self):
         """Get a time entry."""
+        assert self.token is True
+
+    def test_model_fields(self):
+        """Ensure that the database model has all the right fields."""
+        assert self.token is True
+
+    def test_triggering_events(self):
+        """Test ability to respond to triggering events."""
         assert self.token is True
