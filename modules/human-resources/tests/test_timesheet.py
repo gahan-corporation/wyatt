603d58826cf4c732af304f1bdee8886cb4170448
Xander Harris <duchess@gahan-corporation.com>
2017-12-05 04:38:23 +0000

    Scratch that. See #10.

--- modules/human-resources/tests/test_timesheet.py
+++
@@ -1,9 +0,0 @@
-"""Test cases for Timesheet class."""
-
-
-class TestTimesheet(object):
-    """Timesheet test class."""
-    
-    def test_toggl_integration(self):
-        """Make sure we can interact with the Toggl API."""
-        assert False
