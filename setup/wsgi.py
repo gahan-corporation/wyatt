ca8ae7ebb5edf2c31d851f34708272d736da91a5
Xander Harris <xander@ecla.solutions>
2018-01-07 22:56:08 +0000

    Update wsgi. See #2.

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -1,3 +1,5 @@
+"""Gerp WSGI module."""
+# pylint: disable-msg=C0103
 # WSGI Handler sample configuration file.
 #
 # Change the appropriate settings below, in order to provide the parameters
@@ -10,18 +12,16 @@
 #
 # For gunicorn additional globals need to be defined in the Gunicorn section.
 # Then the following command should run:
-#   $ gunicorn odoo:service.wsgi_server.application -c openerp-wsgi.py
+#   $ gunicorn gerp:service.wsgi_server.application -c openerp-wsgi.py
 
-import odoo
+import gerp
 
-#----------------------------------------------------------
 # Common
-#----------------------------------------------------------
-odoo.multi_process = True # Nah!
+gerp.multi_process = True  # Nah!
 
 # Equivalent of --load command-line option
-odoo.conf.server_wide_modules = ['web']
-conf = odoo.tools.config
+gerp.conf.server_wide_modules = ['web']
+conf = gerp.tools.config
 
 # Path to the OpenERP Addons repository (comma-separated for
 # multiple locations)
@@ -29,24 +29,20 @@ conf = odoo.tools.config
 conf['addons_path'] = '../../addons/trunk,../../web/trunk/addons'
 
 # Optional database config if not using local socket
-#conf['db_name'] = 'mycompany'
-#conf['db_host'] = 'localhost'
-#conf['db_user'] = 'foo'
-#conf['db_port'] = 5432
-#conf['db_password'] = 'secret'
+# conf['db_name'] = 'mycompany'
+# conf['db_host'] = 'localhost'
+# conf['db_user'] = 'foo'
+# conf['db_port'] = 5432
+# conf['db_password'] = 'secret'
 
-#----------------------------------------------------------
 # Generic WSGI handlers application
-#----------------------------------------------------------
-application = odoo.service.wsgi_server.application
+application = gerp.service.wsgi_server.application
 
-odoo.service.server.load_server_wide_modules()
+gerp.service.server.load_server_wide_modules()
 
-#----------------------------------------------------------
 # Gunicorn
-#----------------------------------------------------------
 # Standard OpenERP XML-RPC port is 8069
-bind = '127.0.0.1:8069'
+bind = '0.0.0.0:8069'
 pidfile = '.gunicorn.pid'
 workers = 4
 timeout = 240
